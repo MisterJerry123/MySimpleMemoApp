cb711fa235cf1dd2b80e426fe9efed8d869cdbd1
MisterJerry <misterjerry12345@gmail.com>
2025-09-20 16:04:03 +0900

    refactor: 메모 수정 ViewModel의 LiveData 캡슐화 적용

--- app/src/main/java/com/example/memousingroomdb/MemoSharedViewModel.kt
+++ app/src/main/java/com/example/memousingroomdb/MemoSharedViewModel.kt
@@ -1,14 +1,16 @@
 package com.example.memousingroomdb
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.memousingroomdb.db.Memo
 
 class MemoSharedViewModel: ViewModel() {
-    val resultMemo = MutableLiveData<Memo>()
+    private val _resultMemo = MutableLiveData<Memo>()
+    val resultMemo: LiveData<Memo> get() = _resultMemo
 
-    fun postResult(memo:Memo){
-        resultMemo.value = memo
+    fun updateMemo(memo:Memo){
+        _resultMemo.value = memo
     }
 
 }
--- app/src/main/java/com/example/memousingroomdb/UpdateMemoFragment.kt
+++ app/src/main/java/com/example/memousingroomdb/UpdateMemoFragment.kt
@@ -5,15 +5,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.annotation.RequiresApi
-import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import com.example.memousingroomdb.databinding.FragmentDetailMemoBinding
 import com.example.memousingroomdb.databinding.FragmentUpdateMemoBinding
 import com.example.memousingroomdb.db.Memo
-import com.example.memousingroomdb.db.MemoDao
 import com.example.memousingroomdb.db.MemoDatabase
 import java.time.LocalDate
 
@@ -60,7 +56,7 @@ class UpdateMemoFragment : Fragment() {
         binding.btnMemoUpdate.setOnClickListener {
             val newMemo = Memo(id= memo!!.id,title = binding.etTitle.text.toString(), date = "${LocalDate.now()} 수정됨", content = binding.etContent.text.toString())
             db?.memoDao()?.updateMemo(newMemo)
-            sharedViewModel.postResult(newMemo)
+            sharedViewModel.updateMemo(newMemo)
 
 
             requireActivity().supportFragmentManager.popBackStack()
