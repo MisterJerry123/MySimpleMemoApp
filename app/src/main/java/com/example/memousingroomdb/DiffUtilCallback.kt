3b52b5b3495760b9e55b758af4295711e663eb79
MisterJerry <misterjerry12345@gmail.com>
2025-09-23 18:01:26 +0900

    feat: 메모 삭제시 순번 반영 기능 구현

--- app/src/main/java/com/example/memousingroomdb/DiffUtilCallback.kt
+++ app/src/main/java/com/example/memousingroomdb/DiffUtilCallback.kt
@@ -5,7 +5,7 @@ import com.example.memousingroomdb.db.Memo
 
 class DiffUtilCallback:DiffUtil.ItemCallback<Memo>() {
     override fun areItemsTheSame(oldItem: Memo, newItem: Memo): Boolean {
-        return oldItem.hashCode()==newItem.hashCode()
+        return oldItem.id==newItem.id
     }
 
     override fun areContentsTheSame(oldItem: Memo, newItem: Memo): Boolean {
