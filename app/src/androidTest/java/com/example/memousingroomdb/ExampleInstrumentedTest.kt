5bd3701cbbf1fff451dc0a138e5af73f44293a52
MisterJerry <misterjerry12345@gmail.com>
2025-10-07 20:44:49 +0900

    feat: main화면에서 메모 추가하기 버튼을 누른경우 AddMemoActivity가 잘 열리는지 테스트코드 추가

--- app/src/androidTest/java/com/example/memousingroomdb/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/memousingroomdb/ExampleInstrumentedTest.kt
@@ -2,11 +2,19 @@ package com.example.memousingroomdb
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,10 +23,15 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+    @get:Rule
+    val activityRule = ActivityScenarioRule(MainActivity::class.java)
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.memousingroomdb", appContext.packageName)
+    fun 메모_추가하기_버튼을_눌렀을_때_추가하기_화면으로_넘어가는지_확인() {
+        onView(withId(R.id.btn_add))
+            .perform(click())
+
+        onView(withId(R.id.tv_title))
+            .check(matches(isDisplayed()))
     }
 }
