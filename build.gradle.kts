73fb43edddc2584129b5cd0f2577f07a0a676dc3
MisterJerry <misterjerry12345@gmail.com>
2025-10-12 21:09:22 +0900

    chore: AGP버전 및 kotlin 버전, KSP버전 업

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
-    id("com.google.devtools.ksp") version "2.2.10-2.0.2"
+    id("com.google.devtools.ksp") version "2.2.20-2.0.3"
 
     // Add the dependency for the Google services Gradle plugin
     id("com.google.gms.google-services") version "4.4.4" apply false
