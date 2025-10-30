9bbe4a9159c09912cee435c397dfabbe2094ea38
MisterJerry <misterjerry12345@gmail.com>
2025-10-30 20:08:21 +0900

    chore: 패키지 이름 변경

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,14 +7,14 @@ plugins {
 }
 
 android {
-    namespace = "com.example.memousingroomdb"
+    namespace = "com.misterjerry.simplememo"
     compileSdk = 35
     buildFeatures{
         viewBinding = true
     }
 
     defaultConfig {
-        applicationId = "com.example.memousingroomdb"
+        applicationId = "com.misterjerry.simplememo"
         minSdk = 24
         targetSdk = 35
         versionCode = 1
